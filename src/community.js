3daf5d6ba5b8ebddc0e98ff896ccd7e2a3dbfc80
Nathan-Gonzales <nathangonzales114@yahoo.com>
2023-02-02 13:47:12 -0700

    added lore page, started dynamic nav

--- src/community.js
+++ src/community.js
@@ -1,4 +1,5 @@
 import imgBanner from './images/Only_Heroes.jpg';
+import Angelz from './images/Angelz.webp';
 
 const Community = () => {
     return(
@@ -15,12 +16,35 @@ const Community = () => {
                                 This will serve as a place for the existing players to meet each other and also
                                 for new players to find us.
                             </p>
-                            <h1 className='text-neutral-200 text-4xl text-center w-full font-red-rose'>Most Active Guilds in Game</h1>
+                            <h1 className='text-neutral-200 mb-5 text-4xl text-center w-full font-red-rose'>Most Active Guilds in Game</h1>
                         </div>
-                        <section className='text-neutral-200 text-4xl text-center w-full font-red-rose'>
-                            <ul>
-                                <li>Angelz</li>
-                                <li></li>
+                        <section className='bg-neutral-900/[.65] border-neutral-900 border-2 text-neutral-200 text-4xl text-left w-full font-red-rose'>
+                            <ul className='flex flex-col items-center text-center justify-start '>
+                                <li className='p-10'>Angelz
+                                    <img className='border-neutral-900 border-2 mt-3 w-full' src={Angelz} alt="" />
+                                    <h1 className='text-left text-2xl pt-5'>Angelz Discord</h1>
+                                    <h1 className='text-left text-2xl py-5'><a href="https://angelzcommunity.org/"> Angelz Official Website - (angelzcommunity.org)</a></h1>
+                                    <p className='text-left'>
+                                        This is my main guild. It's ran by the wonderful miss Aqua. 
+                                        5 star guild house and allows multiple alt characters.
+                                        Redeemers every week
+                                    </p>
+                                </li>
+                                <li>I Lift Heavy</li>
+                                <li>Cool Beanz</li>
+                                <li>No Fun Allowed</li>
+                                <li>Nightcap</li>
+                                <li>GhostKnights</li>
+                                <li>AngelParty</li>
+                                <li>aLaMuze</li>
+                                <li>Insight</li>
+                                <li>AssassinPie</li>
+                                <li>Empyrean</li>
+                                <li>BeautyBlades</li>
+                                <li>Irregular</li>
+                                <li>KawaiiLollis</li>
+                                <li>LoveClub</li>
+                                <li>LastSeraphim</li>
                                 <li></li>
                             </ul>
                         </section>
--- src/lore.js
+++ src/lore.js
@@ -1,7 +1,37 @@
+import imgBanner from './images/Jarlath.JPG';
+
+
 const Lore = () => {
-
-
-
+    return(
+        <main className='bg-[url("./images/Magicians_Guild.png")] bg-fixed'>
+            <section className='min-h-screen'>
+                <section className='flex items-center justify-center'>
+                    <div className='bg-neutral-900/[.65] flex flex-col h-full justify-between max-w-screen-xl px-3 border-neutral-900 border-x-2'>
+                        <img className='border-neutral-900 border-x-2 border-b-2 mb-10 max-h-60' src={imgBanner} alt="" />
+                        <div className='flex flex-col items-center'>
+                            <h1 className='bg-gradient-to-b from-black/[.65] to-neutral-900/[.65] font-red-rose py-10 w-4/5 text-center text-neutral-200 text-5xl border-neutral-900 border-2 rounded-sm'>
+                                Lore</h1>
+                            <p className='bg-gradient-to-b from-black to-neutral-900 font-red-rose p-10 w-full text-center text-neutral-200 text-3xl  rounded-sm mt-10 border-neutral-900 border-2'>
+                                Our beloved story of the hero who can't catch a break from all the meddling gods.
+                                Also, was Ellis always part of the Silent Brotherhood???
+                            </p>
+                        </div>
+                        <section className='flex flex-col gap-x-10 gap-y-10 py-10 px-36 lg:flex-row lg:flex-wrap'>
+                            <div className='bg-gradient-to-b from-black to-neutral-900 border-neutral-900 border-2 basis-1/3 flex-1 h-96'>Test</div>
+                            <div className='bg-gradient-to-b from-black to-neutral-900 border-neutral-900 border-2 basis-1/3 flex-1 h-96'>Test</div>
+                            <div className='bg-gradient-to-b from-black to-neutral-900 border-neutral-900 border-2 basis-1/3 flex-1 h-96'>Test</div>
+                            <div className='bg-gradient-to-b from-black to-neutral-900 border-neutral-900 border-2 basis-1/3 flex-1 h-96'>Test</div>
+                        </section>
+                        <section className='flex flex-col items-end justify-start mt-5'>
+                            <div className='bg-gradient-to-b from-black/[.65] to-neutral-900/[.65] font-red-rose py-5 w-1/3 text-center text-neutral-200 text-3xl border-neutral-900 border-2 rounded-sm'>
+                                <a href="https://forums.vindictus.nexon.net/categories/story">Link to the official Forum</a>
+                            </div>
+                        </section>
+                    </div>
+                </section>
+            </section>
+        </main>
+    );
 }
 
 export default Lore;
