ab5c82cdf70dde1d86b31b0272d3f5c3c74c05d0
Nathan-Gonzales <nathangonzales114@yahoo.com>
2023-03-08 15:58:44 -0700

    built out simulator UI

--- src/gearSimulator.js
+++ src/gearSimulator.js
@@ -1,27 +1,43 @@
 import imgBanner from './images/FerghusHimself.png';
-import startSimulator from './startSimulator';
+import React, { useState } from 'react';
+import StartSimulator from './StartSimulator';
 
 
 const GearSimulator = () => {
+
+    const [preStart, setPreStart] = useState(true);
+
+    const handleClick = () => {
+        setPreStart(false);
+    }
 
     return (
         <main className='bg-[url("./images/TheForge.jpg")] bg-fixed'>
             <section className='min-h-screen'>
                 <section className='flex items-center justify-center'>
-                    <div className='bg-neutral-900/[.75] flex flex-col h-full justify-between max-w-screen-xl px-3 border-neutral-900 border-x-2'>
+                    <div className='bg-neutral-900/[.75] flex flex-col h-full justify-between max-w-screen-xl w-4/5 px-3 border-neutral-900 border-x-2'>
                         <img className='border-neutral-900 border-x-2 border-b-2 mb-10 max-h-60' src={imgBanner} alt="" />
                         <div className='flex flex-col items-center'>
+                            {preStart && (
                             <h1 className='bg-gradient-to-b from-black/[.65] to-neutral-900/[.65] font-red-rose py-10 w-4/5 text-center text-neutral-200 text-5xl border-neutral-900 border-2 rounded-sm'>
-                                Gear Simulator</h1>
+                                Gear Simulator</h1>)
+                            }
+                            {preStart && (
                             <p className='bg-gradient-to-b from-black to-neutral-900 font-red-rose p-10 w-full text-center text-neutral-200 text-3xl  rounded-sm my-10 border-neutral-900 border-2'>
                                 This system is meant to simulate the in game armor/equipment system. You can see what stats you would obtain
                                 for equipping new gear. For example maybe you need a little more defense and you're wondering how much 
                                 defense you could get from upgrading from Milletian armor to Ardri.
-                            </p>
+                            </p>)
+                            }
+                            {preStart && (
+                            <button className='bg-gradient-to-b from-black/[.65] to-neutral-900/[.65] font-red-rose py-10 w-1/2 text-center text-neutral-200 text-5xl border-neutral-900 border-2 rounded-sm' 
+                            onClick={handleClick}>START
+                            </button>)
+                            }
+                            {!preStart && (
+                                <StartSimulator />
+                            )}
                         </div>
-                        <section className='flex items-center justify-center'>
-                            <button className='bg-gradient-to-b from-black/[.65] to-neutral-900/[.65] font-red-rose py-10 w-1/2 text-center text-neutral-200 text-5xl border-neutral-900 border-2 rounded-sm' onClick={startSimulator}>START</button>
-                        </section>
                     </div>
                 </section>
             </section>
--- src/startSimulator.js
+++ src/startSimulator.js
@@ -1,13 +1,42 @@
-import React, { useState } from 'react';
+import { useEffect } from "react";
 
 
-const startSimulator = () => {
-    // const [] = useState();
+const StartSimulator = () => {
+
+    const stats = Array(18).fill(<div className="">STA: 10</div>)
+    const slots = Array(18).fill(<div className="bg-neutral-600 w-full h-full rounded-md">test</div>)
+    const gear =  Array(30).fill(<div className="bg-neutral-600 w-full h-full rounded-md">test</div>)
+
+    useEffect(() => {
+
+    }, []);
+
 
     return(
-        <></>
+        <section className="flex flex-col w-full">
+            <div className="grid grid-cols-6 text-white">
+                <div className="col-span-1 flex flex-col items-center text-center w-full">
+                    <h1 className="font-red-rose text-white text-4xl pb-10 px-10">Stats</h1>
+                    <div className="grid grid-cols-2 gap-1 bg-gradient-to-b from-black to-neutral-900 p-5 rounded-lg font-red-rose w-full border-neutral-900 border-2">
+                        {stats}
+                    </div>
+                </div>
+                <div className="col-span-2 text-center w-full">
+                    <h1 className="font-red-rose text-white text-4xl pb-10 px-10">Slots</h1>
+                    <div className="grid grid-cols-[repeat(3,_60px)] grid-rows-[repeat(6,_60px)] gap-1 place-items-center justify-center py-5 bg-gradient-to-b from-black to-neutral-900 rounded-lg font-red-rose w-full border-neutral-900 border-2">
+                        {slots}
+                    </div>
+                </div>
+                <div className="col-span-3 text-center w-full">
+                    <h1 className="font-red-rose text-white text-4xl pb-10 px-10">Equip</h1>
+                    <div className="grid grid-cols-[repeat(5,_60px)] grid-rows-[repeat(6,_60px)] gap-1 place-items-center justify-center py-5 bg-gradient-to-b from-black to-neutral-900 rounded-lg font-red-rose w-full border-neutral-900 border-2">
+                        {gear}
+                    </div>
+                </div>
+            </div>        
+        </section>
     );
 }
 
 
-export default startSimulator;
+export default StartSimulator;
